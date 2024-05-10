b424392d64c139bd07d1a78dc1db9790355ca323
Brian Gatt Carbonaro <brian.gatt@sperasoft.com>
2024-05-10 17:21:48 +0200

    Add Target Actor (and Blueprint Class)

---
+++ Source/UnrealEngineCourse/UnrealEngineCourseTarget.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UnrealEngineCourseTarget.h"
+
+// Sets default values
+AUnrealEngineCourseTarget::AUnrealEngineCourseTarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	HitPoints = 100;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetSimulatePhysics(true);
+
+	RootComponent = Mesh;
+
+}
+
+// Called when the game starts or when spawned
+void AUnrealEngineCourseTarget::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AUnrealEngineCourseTarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/UnrealEngineCourse/UnrealEngineCourseTarget.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UnrealEngineCourseTarget.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class UNREALENGINECOURSE_API AUnrealEngineCourseTarget : public AActor
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	UStaticMeshComponent* Mesh;
+	
+public:	
+	// Sets default values for this actor's properties
+	AUnrealEngineCourseTarget();
+	
+	UPROPERTY(EditAnywhere, Category=Health)
+	int32 HitPoints;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
