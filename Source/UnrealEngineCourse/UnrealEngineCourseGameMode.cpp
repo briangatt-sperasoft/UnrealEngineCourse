f7c33276fc16dd80d426c0eea327b42279e96b1d
Brian Gatt Carbonaro <brian.gatt@sperasoft.com>
2024-05-27 16:47:17 +0200

    Update game mode to utilise HUD

--- Source/UnrealEngineCourse/UnrealEngineCourseGameMode.cpp
+++ Source/UnrealEngineCourse/UnrealEngineCourseGameMode.cpp
@@ -4,11 +4,17 @@
 #include "UnrealEngineCourseCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
+#include <GameFramework/HUD.h>
+
 AUnrealEngineCourseGameMode::AUnrealEngineCourseGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// set default HUD class to our Blueprinted HUD
+	static ConstructorHelpers::FClassFinder<AHUD> HUDClassFinder(TEXT("/Game/FirstPerson/Blueprints/HUD/BP_HUD"));
+	HUDClass = HUDClassFinder.Class;
 
 }
